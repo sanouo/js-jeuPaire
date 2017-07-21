14ce6ffd29dcb3ebbfde6e651408db52dbe35e83
sanouo <sanouo591@gmail.com>
2017-07-21 14:13:30 +0200

    create function comparer in the js

--- js/main.js
+++ js/main.js
@@ -2,33 +2,45 @@ var dos = document.getElementsByClassName("carte");
 var image = ["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jpg","img/7.jpg","img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jpg","img/7.jpg"];
 var compteur = 0;
 var valeur = [];
+nbpairOk=0;
 
 for (let i = 0; i < dos.length; i++) {
   dos[i].addEventListener('click',function(){
     if(compteur == 0){
-      // retourne();
+      
       dos[i].src = image[i];
-      valeur.push(this.src);
+      valeur.push(this);
       compteur++;
     } else {
       compteur = 0;
-      // retourne(i);
+
       dos[i].src = image[i];
-      valeur.push(this.src);
-      comparer();
+      valeur.push(this);
+      comparer(this);
     }
   });
 }
 
-// function retourne (i){
-//   dos[i].src = image[i];
-// }
 
-
-function comparer()
+function comparer(element)
 {
-  if (valeur[0] != valeur[1]){
-    alert("perdu");
+  console.log(valeur);
+  if(valeur[0].src != valeur[1].src){
+    //retournemnt
+    console.log("pas les meme");
+    setTimeout(function(){
+      valeur[0].src="img/portrait.png";
+      valeur[1].src="img/portrait.png";
+    }, 1000);
   }
-  else {alert("gagner");}
+  else{
+    //retournemeny
+    nbpairOk++;
+    if (nbpairOk = 7) {
+
+    }
+  }
+
+  valeur = [];
+
 }
