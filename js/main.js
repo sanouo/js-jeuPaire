2bc5426c22f9becef363a72d9745e91128dc033a
sanouo <sanouo591@gmail.com>
2017-07-21 10:48:31 +0200

    create for i n js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 var dos = document.getElementsByClassName("carte");
 var image = ["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jpg","img/7.jpg","img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jpg","img/7.jpg"];
-// parcourir image de dos
-for (let i = 0; i < dos.length; i++) {
-  dos[i].addEventListener('click',retourne);
-  function retourne (){
-    dos[i].src = image[i];
+var compteur = 0;
+var valeur = [];
 
+for (let i = 0; i < dos.length; i++) {
+  dos[i].addEventListener('click',function(){
+    if(compteur == 0){
+      // retourne();
+      dos[i].src = image[i];
+      valeur.push(this.src);
+      compteur++;
+    } else {
+      compteur = 0;
+      // retourne(i);
+      dos[i].src = image[i];
+      valeur.push(this.src);
+      comparer();
+    }
+  });
 }
 
+// function retourne (i){
+//   dos[i].src = image[i];
+// }
+
+
+function comparer()
+{
+  if (valeur[0] != valeur[1]){
+    alert("perdu");
+  }
+  else {alert("gagner");}
 }
